a7faa887763594fa9a879eb34f90b48f2d63390b
Rafael Rodrigues <rafaa.verdao@gmail.com>
2022-01-05 14:27:56 +0100

    Domain Category created

--- src/main/java/com/rafaelrodrigues/cursomc/resources/CategoryResource.java
+++ src/main/java/com/rafaelrodrigues/cursomc/resources/CategoryResource.java
@@ -1,16 +1,24 @@
 package com.rafaelrodrigues.cursomc.resources;
 
+import com.rafaelrodrigues.cursomc.domain.Category;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 
     @GetMapping
-    public String list(){
-        return "Rest is working";
+    public List<Category> list(){
+
+        Category category1 = new Category(1, "Computing");
+        Category category2 = new Category(2, "Office");
+
+        return Arrays.asList(category1,category2);
     }
 
 }
