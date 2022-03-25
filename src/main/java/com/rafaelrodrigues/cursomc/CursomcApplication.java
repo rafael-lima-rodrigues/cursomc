f06cc841b65db274d95b701c4599d8de9e0796f4
rafael.lrodrigues <rafael.lrodrigues@totvs.com.br>
2022-03-24 22:47:09 -0300

    Creating instantiation operation

--- src/main/java/com/rafaelrodrigues/cursomc/CursomcApplication.java
+++ src/main/java/com/rafaelrodrigues/cursomc/CursomcApplication.java
@@ -1,13 +1,30 @@
 package com.rafaelrodrigues.cursomc;
 
+import com.rafaelrodrigues.cursomc.domain.Category;
+import com.rafaelrodrigues.cursomc.repositories.CategoryRespository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRespository categoryRespository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		Category cat1 = new Category(null, "Informatic");
+		Category cat2 = new Category(null, "Office");
+
+		categoryRespository.saveAll(Arrays.asList(cat1,cat2));
+	}
 }
